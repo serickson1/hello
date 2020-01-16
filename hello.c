3e612af24a67bcc069f20a02c910c008f8ed05ad
jp1042016 <59965715+jp1042016@users.noreply.github.com>
2020-01-16 08:47:34 -0600

    Ben added print commands for his name and major

--- hello.c
+++ hello.c
@@ -1,11 +1,14 @@
 /* Scott Erickson, Prints my Name and Major
 Name: Scott
 Date: 2020/01/14
+Collaborator: Ben Rezac
 */
 #include <stdio.h>
 
 int main(void) {
 	printf("Scott Erickson\n");
 	printf("Electical Engineering\n");
+	printf("Ben Rezac\n");
+	printf("Electrical Engineering\n");
 	return 0;
 }
